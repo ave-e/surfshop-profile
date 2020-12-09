7722ab2aa46e8cce7d8bdbcf4bed29107d6a2988
ave-e <keith.hershell@yahoo.com>
2020-12-09 17:24:25 +1100

    Update index.js

--- middleware/index.js
+++ middleware/index.js
@@ -34,38 +34,43 @@ const middleware = {
 		res.redirect('back');
 	},
 	isValidPassword: async (req, res, next) => {
-		const { user } = await User.authenticate()(req.user.username, req.body.currentPassword);
-		if (user) {
-			// add user to res.locals
-			res.locals.user = user;
-			next();
-		} else {
-			req.session.error = 'Incorrect current password!';
-			return res.redirect('/profile');
-		}
-	},
-	changePassword: async (req, res, next) => {
-		const {
-			newPassword,
-			passwordConfirmation
-		} = req.body;
-
-		if (newPassword && !passwordConfirmation) {
-			req.session.error = 'Missing password confirmation!';
-			return res.redirect('/profile');
-		} else if (newPassword && passwordConfirmation) {
-			const { user } = res.locals;
-			if (newPassword === passwordConfirmation) {
-				await user.setPassword(newPassword);
-				next();
-			} else {
-				req.session.error = 'New passwords must match!';
-				return res.redirect('/profile');
-			}
-		} else {
-			next();
-		}
+	const { user } = await User.authenticate()(req.user.username, req.body.currentPassword);
+	if (user) {
+		// add user to res.locals
+		res.locals.user = user;
+		next();
+	} else {
+		middleware.deleteProfileImage(req);
+		req.session.error = 'Incorrect current password!';
+		return res.redirect('/profile');
 	}
+},
+changePassword: async (req, res, next) => {
+	const {
+		newPassword,
+		passwordConfirmation
+	} = req.body;
+
+	if (newPassword && !passwordConfirmation) {
+		middleware.deleteProfileImage(req);
+		req.session.error = 'Missing password confirmation!';
+		return res.redirect('/profile');
+	} else if (newPassword && passwordConfirmation) {
+		const { user } = res.locals;
+		if (newPassword === passwordConfirmation) {
+			await user.setPassword(newPassword);
+			next();
+		} else {
+			middleware.deleteProfileImage(req);
+			req.session.error = 'New passwords must match!';
+			return res.redirect('/profile');
+		}
+	} else {
+		next();
+	}
+},
+deleteProfileImage: async (req) => {
+	if (req.file) await cloudinary.v2.uploader.destroy(req.file.public_id);
 }
 
 module.exports = middleware;
