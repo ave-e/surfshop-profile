77370ba076cadbeba0fd3f90331a4385162cd033
ave-e <keith.hershell@yahoo.com>
2020-12-09 17:33:16 +1100

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -71,17 +71,22 @@ async postRegister(req, res, next) {
 		res.render('profile', { posts });
 	},
 	async updateProfile(req, res, next) {
-		const {
-			username,
-			email
-		} = req.body;
-		const { user } = res.locals;
-		if (username) user.username = username;
-		if (email) user.email = email;
-		await user.save();
-		const login = util.promisify(req.login.bind(req));
-		await login(user);
-		req.session.success = 'Profile successfully updated!';
-		res.redirect('/profile');
+	const {
+		username,
+		email
+	} = req.body;
+	const { user } = res.locals;
+	if (username) user.username = username;
+	if (email) user.email = email;
+	if (req.file) {
+		if (user.image.public_id) await cloudinary.v2.uploader.destroy(user.image.public_id);
+		const { secure_url, public_id } = req.file;
+		user.image = { secure_url, public_id };
 	}
+	await user.save();
+	const login = util.promisify(req.login.bind(req));
+	await login(user);
+	req.session.success = 'Profile successfully updated!';
+	res.redirect('/profile');
+}
 }
