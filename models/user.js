3ba658682061ddafcee113410900923263cfdf67
Ian Schoonover <6356890+nax3t@users.noreply.github.com>
2020-12-10 20:18:27 -0600

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,8 +5,8 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
 	email: { type: String, unique: true, required: true },
 	image: {
-	secure_url: { type: String, default: '/images/default-profile.jpg' },
-	public_id: String
+	path: { type: String, default: '/images/default-profile.jpg' },
+	filename: String
 }
 });
 
